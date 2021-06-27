8b95a2131e4bef916ac3ba8fd16088633a07546b
dongdalee <wjd1wo30821@naver.com>
2021-06-27 18:20:32 +0900

    finial

--- network/create_network.sh
+++ network/create_network.sh
@@ -4,4 +4,5 @@ ENV_LOCATION=$PWD/.env
 echo $ENV_LOCATION
 source $ENV_LOCATION
 
-docker network create --driver overlay --subnet=118.67.128.0/24 --attachable "$NETWORK_NAME"
+#docker network create --driver overlay --subnet=118.67.128.0/24 --attachable "$NETWORK_NAME"
+docker network create --driver overlay --subnet=192.168.136.131/24 --attachable "$NETWORK_NAME"
